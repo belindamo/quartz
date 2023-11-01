7c189969ccb37eea784cb58b0db2f2fea5c925f9
totoandindra <intheindranet@gmail.com>
2023-11-01 12:43:51 -0700

    Quartz sync: Nov 1, 2023, 12:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🧚🏼‍♀️ Indra's systems",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://totoandindra.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
